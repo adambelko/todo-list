17bad0aaaa2d7a4a2396119cbae36bb8f345934a
Adzi <belkoadam@gmail.com>
2022-10-26 20:41:22 +1100

    functionality for removing projects from nav list added + few other minor things

--- src/project.js
+++ src/project.js
@@ -1,12 +1,12 @@
 const loadEventListeners = () => {
     const addNewProject = document.querySelectorAll(".nav__add-new-project");
-    addNewProject.forEach(el => el.addEventListener("click", showProjectForm));
+    addNewProject.forEach((el) => el.addEventListener("click", showProjectForm));
 
     const projectFormCancellBtn = document.querySelector(".nav__form-cancell-btn");
     projectFormCancellBtn.addEventListener("click", hideProjectForm);
 
     const projectFormAddBtn = document.querySelector(".nav__form-add-btn");
-    projectFormAddBtn.addEventListener("click", addProject);
+    projectFormAddBtn.addEventListener("click", (e) => addProject(e));
 };
 
 const showProjectForm = () => {
@@ -18,30 +18,50 @@ const showProjectForm = () => {
 const hideProjectForm = () => {
     const projectForm = document.querySelector(".nav__form");
     projectForm.classList.remove("nav__form--active");
+    clearFormInput();
 };
 
-const addProject = () => {
+const addProject = (e) => {
+    e.preventDefault();
     const navProjects = document.querySelector(".nav__section-projects");
     const newProject = document.createElement("li");
     newProject.className = "nav__item";
+    navProjects.appendChild(newProject);
 
     const projectIcon = document.createElement("img");
     projectIcon.className = "nav__icon";
     projectIcon.src = "../dist/img/nav_project.png";
+    newProject.appendChild(projectIcon);
 
     const projectTextDiv = document.createElement("div");
     projectTextDiv.className = "nav__text";
-
-    const formInput = document.querySelector(".nav__form-input");
-    const projectName = document.createTextNode(formInput.value);
-    hideProjectForm();
-
-    navProjects.appendChild(newProject);
-    newProject.appendChild(projectIcon);
     newProject.appendChild(projectTextDiv);
-    projectTextDiv.appendChild(projectName);
+    projectTextDiv.appendChild(getProjectName());
+
+    const projectRemoveIcon = document.createElement("img");
+    projectRemoveIcon.className = "nav__remove-icon";
+    projectRemoveIcon.src = "../dist/img/nav_remove.png";
+    projectRemoveIcon.addEventListener("click", (e) => removeProject(e));
+    newProject.appendChild(projectRemoveIcon);
+
+    hideProjectForm();
 };
 
+const removeProject = (e) => {
+    const navProjects = document.querySelector(".nav__section-projects");
+    const selectedProject = e.target.parentNode;
+    navProjects.removeChild(selectedProject);
+};
+
+const getProjectName = () => {
+    const formInput = document.querySelector(".nav__form-input");
+    return document.createTextNode(makeFirstLetterCap(formInput.value));
+};
+
+const makeFirstLetterCap = (formInput) => {
+    return formInput.charAt(0).toUpperCase() + formInput.slice(1);
+};
+
+const clearFormInput = () => document.querySelector(".nav__form-input").value = "";
 
 export default loadEventListeners;
-
