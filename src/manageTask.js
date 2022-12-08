398b577f66e86abbf971668e502b2063e2d477c2
Adzi <belkoadam@gmail.com>
2022-12-08 21:29:30 +1100

    giving task a priority value working, NavTab priority yet to be done

--- src/manageTask.js
+++ src/manageTask.js
@@ -1,19 +1,40 @@
 import { projectList, saveToMemory } from "./createProject";
 
-const prioritiseTask = () => {
-    console.log("hey")
+const prioritiseTask = (e, taskUUID) => {
+    let taskIndex;
+    let projectIndex;
+
+    for (let i = 0; i < projectList.length; i++) {
+        let findIndex = projectList[i].taskList.findIndex((task) => task.uuid === taskUUID);
+        if (findIndex >= 0) {
+            taskIndex = findIndex;    
+            projectIndex = i;
+            break;
+        }
+    }
+    let importance = projectList[projectIndex].taskList[taskIndex].important;
+
+    if (importance === false) {
+        e.target.classList.add("task-item__important-icon--active");
+        projectList[projectIndex].taskList[taskIndex].important = true;
+        
+    } else {
+        e.target.classList.remove("task-item__important-icon--active");
+        projectList[projectIndex].taskList[taskIndex].important = false;
+    }
+    
+    saveToMemory();
 };
 
 const editTask = () => {
     //code to come  
 };
 
-const removeTask = (e) => {
+const removeTask = (e, taskUUID) => {
     const projectUUID = document.querySelector(".nav__item--active").dataset.uuid;
     const projectIndex = projectList.findIndex(
         (project) => project.uuid === projectUUID);
 
-    const taskUUID = e.target.parentNode.parentNode.dataset.uuid;
     const taskIndex = projectList[projectIndex].taskList.findIndex(
         (task) => task.uuid === taskUUID);
 
